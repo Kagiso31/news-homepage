2971d9e16bdd1f6a6c193fda33f7d577b5d966c8
Kagiso31 <kagisomokou@gmail.com>
2022-11-15 14:35:24 +0200

    Add 'hero and new' active states

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="post">
-    <h2 class="post__heading">{{ title }}</h2>
+    <h2 class="post__heading">
+      <a href="#">{{ title }}</a>
+    </h2>
     <p class="post__body">{{ body }}</p>
   </div>
 </template>
